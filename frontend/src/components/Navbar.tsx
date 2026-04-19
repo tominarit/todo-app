d3a454c1087fc2be16d80e953871964ce28ab154
Toshiaki Tominari <tominarit@gmail.com>
2026-04-19 10:50:23 +0900

    added signin and signup pages

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import { useAuth, UserButton } from '@clerk/react';
+import { Link } from 'react-router';
+
 export default function Navbar() {
+  const { isSignedIn } = useAuth()
+
   return (
-    <nav className="bg-gray-800 p-4 text-white ">
-      <h1 className="text-xl font-bold">Todo App</h1>
+    <nav className="flex items-center justify-between bg-gray-800 p-4 text-white">
+      <Link to="/">
+        <h1 className="text-xl font-bold">Todo App</h1>
+      </Link>
+      {isSignedIn && <UserButton />}
     </nav>
+    
   )
 };
